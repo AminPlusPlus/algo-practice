497b4906213c48185c63a4a8b4e5aa36f1bc9341
Amin <amin030296@gmail.com>
2020-04-29 15:13:52 -0500

    Initial commit

---
+++ src/com/amin610743/Main.java
@@ -0,0 +1,8 @@
+package com.amin610743;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/amin610743/tree/BinaryTree.java
@@ -0,0 +1,4 @@
+package com.amin610743.tree;
+
+public class BinaryTree {
+}
