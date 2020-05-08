50b5aad397697e68b87940fc7baa4381434062f1
Amin <amin030296@gmail.com>
2020-05-08 13:20:15 -0500

    TopSort

--- src/com/amin610743/Main.java
+++ src/com/amin610743/Main.java
@@ -1,20 +1,31 @@
 package com.amin610743;
 
+
 import com.amin610743.graph.Graph;
 
 public class Main {
-
     public static void main(String[] args) {
         Graph graph = new Graph();
-        graph.addNode("MUM");
-        graph.addNode("Walmart");
-        graph.addNode("GoldenDome");
-        graph.addEdge("MUM","Walmart");
-        graph.addEdge("MUM","GoldenDome");
-        graph.removeNode("MUM");
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("D");
+        graph.addNode("C");
 
-        graph.addEdge("GoldenDome","Walmart");
+        graph.addEdge("A","B");
+        graph.addEdge("A","D");
+
+        graph.addEdge("B","C");
+        graph.addEdge("B","D");
 
         graph.print();
+
+
+        System.out.println(graph.topSort("A"));;
+        //graph.depthSearchIterative("X");
+        //graph.breathSearchFirst("MUM");
+
+
+
+
     }
 }
--- src/com/amin610743/graph/Graph.java
+++ src/com/amin610743/graph/Graph.java
@@ -1,8 +1,9 @@
 package com.amin610743.graph;
-
-import org.w3c.dom.Node;
-
 import java.util.*;
+
+/**
+ * @Author Aminjoni Abdullozoda
+ */
 
 public class Graph {
 
@@ -66,8 +67,104 @@ public class Graph {
 
     }
 
+    public void depthSearchIterative(String root) {
+
+        //Null Check
+        GraphNode currentNode =  nodes.get(root);
+        if (currentNode == null)
+            return;
+
+        //node stack holding vertex
+        Stack<GraphNode> nodeStack = new Stack<>();
+        Set<GraphNode> visitedNode = new HashSet<>();
+
+        nodeStack.push(currentNode);
+
+        while(!nodeStack.isEmpty()) {
+            GraphNode tempNode = nodeStack.pop();
+
+            if (visitedNode.contains(tempNode))
+                continue;
+
+            visitedNode.add(tempNode);
+            System.out.println(tempNode);
+
+            for (GraphNode n : adjacencyList.get(tempNode))
+                if(!visitedNode.contains(n))
+                    nodeStack.push(n);
+
+        }
+    }
 
 
+    /**
+     *
+     * @param root
+     * @return
+     */
+    public List<String> topSort(String root) {
+        GraphNode node = nodes.get(root);
+
+        Stack<GraphNode> sortNode = new Stack<>();
+
+        if (node == null)
+            throw new IllegalArgumentException();
+
+        topSort(node,new HashSet<>(),sortNode);
+
+        List<String> sortedString = new ArrayList<>();
+        while (!sortNode.isEmpty()){
+            sortedString.add(sortNode.pop().getLabel());
+        }
+        return sortedString;
+    }
+
+    private void topSort(GraphNode root, Set<GraphNode> visited,
+                         Stack<GraphNode> sortNode) {
+        visited.add(root);
+
+        for (GraphNode node : adjacencyList.get(root)) {
+            if (!visited.contains(node))
+                topSort(node,visited,sortNode);
+        }
+
+        //adding node
+        sortNode.push(root);
+    }
+
+    /**
+     *
+     * @param s
+     * @return
+     */
+    public void breathSearchFirst(String s) {
+
+        GraphNode node = nodes.get(s);
+        if (node == null)
+            return;
+
+        Set<GraphNode> visited = new HashSet<>();
+        Queue<GraphNode> onLine =  new LinkedList<>();
+        onLine.add(node);
+
+        while (!onLine.isEmpty()){
+
+            GraphNode tempNode = onLine.remove();
+
+            if (visited.contains(tempNode))
+                continue;
+
+            visited.add(tempNode);
+            System.out.println(tempNode);
+
+            for (GraphNode n : adjacencyList.get(tempNode)) {
+                if (!visited.contains(n))
+                onLine.add(n);
+            }
+        }
+
+
+    }
 
    public void print() {
         for (GraphNode source : adjacencyList.keySet()) {
